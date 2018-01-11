7bb815832f443137716a133391c83924aa7bcaa1
Gerald Ting <horzon16@gmail.com>
2018-01-11 21:17:05 +0800

    Update codes

---
+++ entertainment_center.py
@@ -0,0 +1,29 @@
+# It's the main program to create the web page
+# Run "python entertainment_center.py" to create the HTML web page
+# Created by Gerald Ding (horzon16@gmail.com)
+
+# import the relative programs
+import media
+import fresh_tomatoes
+
+# call the function Movie in media class
+
+# The Martian -- information
+the_martian = media.Movie("The Martian",
+			"https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg", 
+			"https://youtu.be/ej3ioOneTy8")
+
+# Sully -- information
+sully = media.Movie("Sully",
+			"https://upload.wikimedia.org/wikipedia/en/8/82/Sully_xxlg.jpeg",
+			"https://youtu.be/9uhuyyY5WuU")
+
+# Your Name -- information
+your_name = media.Movie("Your Name",
+			"https://upload.wikimedia.org/wikipedia/en/0/0b/Your_Name_poster.png",
+			"https://youtu.be/s0wTdCQoc2k")
+
+# Catogory of the movies
+movies = [the_martian, sully, your_name]
+# using fresh_tomatoes to create the web page
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,9 @@
+# Created by Gerald Ding (horzon16@gmail.com)
+
+# define the Movie class
+class Movie():
+	def __init__(mov, movie_title, poster_image, trailer_youtube):
+		mov.title = movie_title
+		mov.poster_image_url = poster_image
+		mov.trailer_youtube_url = trailer_youtube
+
